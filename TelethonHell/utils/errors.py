5e93fa1832eed09178d2b11d8404ca0330e058fd
SHIVANSH XD <149559899+SHIVANSH474@users.noreply.github.com>
2024-01-01 03:33:53 +0530

    Update errors.py

--- TelethonHell/utils/errors.py
+++ TelethonHell/utils/errors.py
@@ -17,7 +17,7 @@ def errors_handler(func):
 
             text = "**Hêllẞø† CRASH REPORT**\n\n"
 
-            link = "[here](https://t.me/ForGo10God)"
+            link = "[here](https://t.me/SHIVANSH39)"
             text += "If you wanna you can report it"
             text += f"- just forward this message {link}.\n"
             text += "Nothing is logged except the fact of error and date\n"
